8a0b73dcfc1622e7ec321fd4b395f56df303ff8c
Banu ispir <ispirbanu@gmail.com>
2021-12-30 00:44:49 +0300

    güncellendi

--- storage.js
+++ storage.js
@@ -1,111 +1,38 @@
-//ES6
-class Storage{
-   static addFilmStorage(newFilm){
-        //storage üzerindeki 
-      let films=this.getFilmsStorage(); //storage'da bulunan diziyi al
-    
-      films.push(newFilm); //filmi ekle 
-      //film dizisine objeler gönderiliyor. 
-        localStorage.setItem("films",JSON.stringify(films)); 
-        //JSON.stringfy()
-        //JSON.stringify  JavaScript nesnesini JSON metnine dönüştürür ve bunu JSON metni olarak bir dizide saklar
-        //JSON-bir nesne/diziden bir JSON dizesi oluşturmaktır.
-    }
-    static getFilmsStorage(){
-        let films;
-        //gönderilen değere karşılık gelen bir key var mı 
-        if(localStorage.getItem("films")===null){ //key yok -> boş bir array oluştur
-            films=[];
-        }
-        else{
-            films=JSON.parse(localStorage.getItem("films")); //key var ->  bu değeri al ve string ifadeye çevir.
-        //JSON (JavaScript Object Notation) 
-        //JSON.parse bir JSON metni dizesini bir JavaScript nesnesine dönüştürür
-        }
-        return films;
-    }
-    
-    //filmi storage dan kaldırma
-    static deleteFilmFromStorage(Filmtitle){
-    
-        let films=this.getFilmsStorage(); 
-        
-        //storage içerisinde bulunan tüm filmler içerisinde gezinerek aynı isme sahip olan filmin indexi alınır ve bu indexe göre silme işlemi yapılır.
-        Filmtitle=Filmtitle.toUpperCase().trim();
-        console.log(Filmtitle);
-        films.forEach(function (film,index) {
-            film=film.title.toUpperCase().trim();
-            if(film===Filmtitle){    //!!if bloğuna girmiyor-> çözüm trim özelliği çalıştırılmalı 
-                                                    //!!!  silme işleminden sonra bir önceki elemanı da siliyor -> çözüm hepsini büyük harfe çevir ve trim kullan
-                films.splice(index,1);
-            }
-    
-        });
-    
-        localStorage.setItem("films",JSON.stringify(films));  //silindikten sonra localStorage güncellenir
-    }
-    
-    //bütün filmleri storage dan kaldırma
-    static clearAllFilmsFromStorage(){
-        localStorage.removeItem("films"); //films anahtarına sahip tüm değerleri LocalStorage üzerinden siler
-    
-    }
+function Storage(){
 }
-
-
-
-
-//ES6 öncesi 
-// function Storage(){
-
-// }
-// Storage.prototype.addFilmStorage= function(newFilm){
-//     //storage üzerindeki 
-//   let films=this.getFilmsStorage(); //storage'da bulunan diziyi al
-
-//   films.push(newFilm); //filmi ekle 
-//   //film dizisine objeler gönderiliyor. 
-//     localStorage.setItem("films",JSON.stringify(films)); 
-//     //JSON.stringfy()
-//     //JSON.stringify  JavaScript nesnesini JSON metnine dönüştürür ve bunu JSON metni olarak bir dizide saklar
-//     //JSON-bir nesne/diziden bir JSON dizesi oluşturmaktır.
-// }
-// Storage.prototype.getFilmsStorage=function(){
-//     let films;
-//     //gönderilen değere karşılık gelen bir key var mı 
-//     if(localStorage.getItem("films")===null){ //key yok -> boş bir array oluştur
-//         films=[];
-//     }
-//     else{
-//         films=JSON.parse(localStorage.getItem("films")); //key var ->  bu değeri al ve string ifadeye çevir.
-//     //JSON (JavaScript Object Notation) 
-//     //JSON.parse bir JSON metni dizesini bir JavaScript nesnesine dönüştürür
-//     }
-//     return films;
-// }
-
-// //filmi storage dan kaldırma
-// Storage.prototype.deleteFilmFromStorage=function(Filmtitle){
-
-//     let films=this.getFilmsStorage(); 
-    
-//     //storage içerisinde bulunan tüm filmler içerisinde gezinerek aynı isme sahip olan filmin indexi alınır ve bu indexe göre silme işlemi yapılır.
-//     Filmtitle=Filmtitle.toUpperCase().trim();
-//     console.log(Filmtitle);
-//     films.forEach(function (film,index) {
-//         film=film.title.toUpperCase().trim();
-//         if(film===Filmtitle){    //!!if bloğuna girmiyor-> çözüm trim özelliği çalıştırılmalı 
-//                                                 //!!!  silme işleminden sonra bir önceki elemanı da siliyor -> çözüm hepsini büyük harfe çevir ve trim kullan
-//             films.splice(index,1);
-//         }
-
-//     });
-
-//     localStorage.setItem("films",JSON.stringify(films));  //silindikten sonra localStorage güncellenir
-// }
-
-// //bütün filmleri storage dan kaldırma
-// Storage.prototype.clearAllFilmsFromStorage=function(){
-//     localStorage.removeItem("films"); //films anahtarına sahip tüm değerleri LocalStorage üzerinden siler
-
-// }
+Storage.prototype.addToStorage= function(newFilm){
+    //Storage üzerinde bulunan diziyi almak için
+    let films=this.getStorage();
+    films.push(newFilm);
+    //Js nesnesini JSON metnine dönüştürdük. 
+    localStorage.setItem("films",JSON.stringify(films));
+}
+Storage.prototype.getStorage=function(){
+    let films;
+    //films adında dizi localstorage içerisinde yoksa boş bir dizi oluştur
+    if(localStorage.getItem("films")===null){
+        films=[];
+    }
+    //varsa bunu dizi olarak al
+    else{
+        films=JSON.parse(localStorage.getItem("films"));
+        //JSON.parse ile bir JSON dizesini Js nesnesine dönüştürdü.
+    }
+    return films;
+}
+Storage.prototype.deleteFilmFromStorage=function(filmAdi){
+    let films=this.getStorage(); 
+    //storage içerisinde bulunan tüm filmler içerisinde gezinerek aynı isme sahip olan
+    //filmin indexi alınır ve bu indexe göre silme işlemi yapılır.
+    filmAdi=filmAdi.toUpperCase().trim();
+    films.forEach(function (film,index) {
+        film=film.title.toUpperCase().trim(); 
+        if(film===filmAdi){    //!!if bloğuna girmiyor-> çözüm trim özelliği çalıştırılmalı                                        //!!!  silme işleminden sonra bir önceki elemanı da siliyor -> çözüm hepsini büyük harfe çevir ve trim kullan
+            films.splice(index,1);
+        }
+    });
+    localStorage.setItem("films",JSON.stringify(films));  //silindikten sonra localStorage güncellenir
+}
+Storage.prototype.clearAllFilmsFromStorage=function(){
+    localStorage.removeItem("films"); //films anahtarına sahip tüm değerleri LocalStorage üzerinden siler
+}
--- ui.js
+++ ui.js
@@ -1,11 +1,8 @@
+function UI(){
 
-//ES6 ile
-class UI{
-//filmi arayüze ekleme
-static addFilmToUi(newFilm){
-    // console.log(newFilm);
-
-    //oluşturulacak tablo yapısı
+}
+UI.prototype.addFilmToUi=function(newFilm){
+     //oluşturulacak tablo yapısı
     /*<tbody id = "films">
     { <!-- <tr>
         <td><img src="" class="img-fluid img-thumbnail"></td>
@@ -14,12 +11,8 @@ static addFilmToUi(newFilm){
         <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
       </tr> -->
     */
-
-   //film bilgi tabloların bulunduğu element
     const filmList= document.getElementById("films");
-    
-
-    //filmleri eklemek
+     //filmleri eklemek
         //1-filmin Resmi 
         //2-filmin adı 
         //3-filmin yönetmeni
@@ -33,20 +26,17 @@ static addFilmToUi(newFilm){
             <td> ${newFilm.genre} </td>
             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
         </tr>
-    
     `;
 }
-
 // İnputları temizleme
-static clearAllInput(element1,element2,element3, element4){
+UI.prototype.clearAllInput= function(element1,element2,element3, element4){
     element1.value="";
     element2.value="";
     element3.value="";
     element4.value="";
 }
-
-// Uyarı, hata mesajları yazdırma
-static Messages(message,type){
+UI.prototype.displayMessages =function(message,type){
+    //mesaj elementini eklemek için
     const cardbody=document.querySelectorAll(".card-body")[0];
     //Alert divi oluşturma
     const div= document.createElement("div");
@@ -57,10 +47,7 @@ static Messages(message,type){
         div.remove();
     },1000);
 }
-
-
-//form yüklendiğinde storage üzerinde bulunan filmleri arayüzde göster
-static loadAllFilms(films){
+UI.prototype.loadAllFilms=function(films){
     const filmList=document.getElementById("films");
     //filmlerin bulunduğu listede gezinerek hepsini arayüzde göster
     films.forEach(film => {
@@ -74,130 +61,14 @@ static loadAllFilms(films){
     </tr>
         `;
     });
-
 }
-
-
-
-static clearAllFilmsFromUi(){
-    const filmList= document. getElementById("films");
-     //filmList.innerHTML=""; //1.yöntem ancak bütük projeler için yavaş
-
-    while(filmList.firstElementChild !==null){ //2. yöntem
-        filmList.firstElementChild.remove();
-    }
-
-}
-
-
-static deleteFilmFromUi(target){
+UI.prototype.deleteFilmFromUi=function(target){
     target.parentElement.parentElement.remove();
 }
+UI.prototype.clearAllFilmsFromUi=function(){
+    const filmList= document.getElementById("films");
+    //filmList.innerHTML=""; //1.yöntem ancak bütük projeler için yavaş
+   while(filmList.firstElementChild !==null){ //2. yöntem
+       filmList.firstElementChild.remove();
+   }
 }
-
-
-
-
-
-
-
-
-
-
-
-//prototype ile ES6 öncesi
-// function UI(){
-// }
-
-// //filmi arayüze ekleme
-// UI.prototype.addFilmToUi=function(newFilm){
-//     // console.log(newFilm);
-
-//     //oluşturulacak tablo yapısı
-//     /*<tbody id = "films">
-//     { <!-- <tr>
-//         <td><img src="" class="img-fluid img-thumbnail"></td>
-//         <td></td>
-//         <td></td>
-//         <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-//       </tr> -->
-//     */
-
-//    //film bilgi tabloların bulunduğu element
-//     const filmList= document.getElementById("films");
-    
-
-//     //filmleri eklemek
-//         //1-filmin Resmi 
-//         //2-filmin adı 
-//         //3-filmin yönetmeni
-//         //4-filmin türü
-//     //+= kullanılacak önceki verilerin silinmemesi birbirinin ardına eklenmesi için
-//     filmList.innerHTML +=`
-//         <tr>
-//             <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
-//             <td> ${newFilm.title} </td>
-//             <td> ${newFilm.director} </td>
-//             <td> ${newFilm.genre} </td>
-//             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-//         </tr>
-    
-//     `;
-// }
-
-// // İnputları temizleme
-// UI.prototype.clearAllInput= function(element1,element2,element3, element4){
-//     element1.value="";
-//     element2.value="";
-//     element3.value="";
-//     element4.value="";
-// }
-
-// // Uyarı, hata mesajları yazdırma
-// UI.prototype.Messages=function(message,type){
-//     const cardbody=document.querySelectorAll(".card-body")[0];
-//     //Alert divi oluşturma
-//     const div= document.createElement("div");
-//     div.className=`alert alert-${type}`;
-//     div.textContent=message;
-//     cardbody.appendChild(div);
-//     setTimeout(function(){
-//         div.remove();
-//     },1000);
-// }
-
-
-// //form yüklendiğinde storage üzerinde bulunan filmleri arayüzde göster
-// UI.prototype.loadAllFilms= function(films){
-//     const filmList=document.getElementById("films");
-//     //filmlerin bulunduğu listede gezinerek hepsini arayüzde göster
-//     films.forEach(film => {
-//         filmList.innerHTML+=`
-//         <tr>
-//         <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
-//         <td> ${film.title} </td>
-//         <td> ${film.director} </td>
-//         <td> ${film.genre} </td>
-//         <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-//     </tr>
-//         `;
-//     });
-
-// }
-
-
-
-// UI.prototype.clearAllFilmsFromUi= function(){
-//     const filmList= document. getElementById("films");
-//      //filmList.innerHTML=""; //1.yöntem ancak bütük projeler için yavaş
-
-//     while(filmList.firstElementChild !==null){ //2. yöntem
-//         filmList.firstElementChild.remove();
-//     }
-
-// }
-
-
-// UI.prototype.deleteFilmFromUi= function(target){
-//     target.parentElement.parentElement.remove();
-// }
